796ff7d1c8222c013e0da2f8da83580eb2d058c7
lanarimarco <marco.lanari@smeup.com>
2020-06-08 22:36:52 +0200

    Adeguamento alla nuova versione di DBNativeAccess

--- src/test/java/com/smeup/testdbnativeaccess/SimpleTest.java
+++ src/test/java/com/smeup/testdbnativeaccess/SimpleTest.java
@@ -6,20 +6,25 @@
 package com.smeup.testdbnativeaccess;
 
 import com.smeup.dbnative.BigintType;
+import com.smeup.dbnative.ConnectionConfig;
 import com.smeup.dbnative.DBField;
-import com.smeup.dbnative.DBMetadata;
+import com.smeup.dbnative.DBNativeAccessConfig;
+import com.smeup.dbnative.FileMetadata;
 import com.smeup.dbnative.SmallintType;
 import com.smeup.dbnative.VarcharType;
 import com.smeup.dbnative.file.DBFile;
 import com.smeup.dbnative.file.Record;
 import com.smeup.dbnative.file.RecordField;
 import com.smeup.dbnative.file.Result;
+import com.smeup.dbnative.manager.DBFileFactory;
 import com.smeup.dbnative.sql.SQLDBMManager;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import org.junit.After;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -27,16 +32,22 @@ import org.junit.Test;
  * @author marco.lanari
  */
 public class SimpleTest {
-    
-    private static final String LIB = "LIBRARY_TEST";
+
     private static final String FILE = "FILE_TEST";
-    private static SQLDBMManager DB;
-    
+    private SQLDBMManager manager;
+    private DBFileFactory fileFactory;
+
     /**
-     *Mi creo una istanza di DB con la libreria LIB ed il file FILE
+     * Mi creo una istanza di DB con la libreria LIB ed il file FILE
      */
-    @BeforeClass
-    public static void createDBMManagerInstance() {
+    @Before
+    public void setUp() {
+        ConnectionConfig config
+          = new ConnectionConfig(FILE, "jdbc:hsqldb:mem:TEST", "sa", "sa", new HashMap<String, String>());
+        manager = new SQLDBMManager(config);
+        fileFactory = new DBFileFactory(new DBNativeAccessConfig(Arrays.asList(config)), (p1) -> {
+            return p1;
+        });
         List<DBField> fields = new ArrayList<>();
         //definisco ID primary key
         fields.add(new DBField("ID", BigintType.INSTANCE, true, true, true));
@@ -44,45 +55,46 @@ public class SimpleTest {
         fields.add(new DBField("NAME", new VarcharType(50), false, false, true));
         //definisco campo RATING che puà essere null
         fields.add(new DBField("RATING", SmallintType.INSTANCE, false, false, false));
-        DBMetadata  metadata = new DBMetadata(FILE, fields);
-        DB = new SQLDBMManager();
-        String url = "jdbc:hsqldb:mem:";
-        DB.setConnectionData(url, "sa", "sa");
-        if (!DB.existLib(LIB)) {
-            DB.createLib(LIB);
-        }
-        if (!DB.existFile(LIB, FILE)) {
-            DB.createFile(LIB, metadata);
+        FileMetadata metadata = new FileMetadata(FILE, fields);
+
+        if (!manager.existFile(FILE)) {
+            manager.createFile(metadata);
         }
     }
-    
+
     @Test
-    public void run() {
+    public void testWrite() {
         //Apro il file
-        DBFile dbFile = DB.openFile(LIB, FILE);
-        
-        //Creo il primo record
-        Record r1 = new Record(
-                new RecordField("ID", "1"), 
-                new RecordField("NAME", "PAPERONE"), 
-                new RecordField("RATING", "0")
-        );
-        //lo scrivo
-        Result w1 = dbFile.write(r1);
-        
-        //Creo il secondo record
-        Record r2 = new Record(
-                new RecordField("ID", "2"), 
-                new RecordField("NAME", "PIPPO"), 
-                new RecordField("RATING", "5")
-        );
-        //lo scrivo
-        Result w2 = dbFile.write(r2);
-        
-        Assert.assertTrue("Mi posiziono su record con ID=2", dbFile.setll(Arrays.asList(new RecordField("ID", "2"))));
-        Assert.assertEquals("Leggo il record corrente", w2, dbFile.read());
-        Assert.assertEquals("Leggo il precedente", w1, dbFile.readPrevious());
-        DB.closeFile(LIB, FILE);
+        try (DBFile dbFile = fileFactory.open(FILE)) {
+            //Creo il primo record
+            Record r1 = new Record(
+              new RecordField("ID", "1"),
+              new RecordField("NAME", "PAPERONE"),
+              new RecordField("RATING", "0")
+            );
+            //lo scrivo
+            Result w1 = dbFile.write(r1);
+
+            //Creo il secondo record
+            Record r2 = new Record(
+              new RecordField("ID", "2"),
+              new RecordField("NAME", "PIPPO"),
+              new RecordField("RATING", "5")
+            );
+            //lo scrivo
+            Result w2 = dbFile.write(r2);
+
+            Assert.assertTrue("Mi posiziono su record con ID=2", dbFile.setll(Arrays.asList(new RecordField("ID", "2"))));
+            Assert.assertEquals("Leggo il record corrente", w2, dbFile.read());
+            Assert.assertEquals("Leggo il precedente", w1, dbFile.readPrevious());
+        }
     }
     
+    @After
+    public void tearDown() {
+        manager.close();
+        fileFactory.close();
+    }
+    
+
 }
